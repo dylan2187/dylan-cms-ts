9ca6b3978a4ca031618f7adb10be6530b76d203e
neodylan <neodylan@foxmail.com>
2023-05-27 13:42:52 +0800

    代码规范 格式化配置 prettier

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface Ikun {
+  name: string
+  age: 18
+}
+
+declare module '*.vue' {
+  import { DefineComponent } from 'vue'
+  const component: DefineComponent
+  export default component
+}
