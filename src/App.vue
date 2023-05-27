4d0d102746e521cc307a15162038158c1955fcd0
neodylan <neodylan@foxmail.com>
2023-05-27 22:22:18 +0800

    App宽高铺满

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="app">
     <h2>app</h2>
+    <router-link to="/main">mian</router-link>
+    <router-link to="/login">login</router-link>
+    <router-view></router-view>
   </div>
-  <router-link to="/main">mian</router-link>
-  <router-link to="/login">login</router-link>
-  <router-view></router-view>
 </template>
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+.app {
+  height: 100vh;
+  width: 100vw;
+  background-color: skyblue;
+}
+</style>
