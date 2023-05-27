5d0eddadc59ee1e27152790f92205a33f626c72a
neodylan <neodylan@foxmail.com>
2023-05-27 14:20:11 +0800

    eslint配置 CSS样式重置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,12 +7,14 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: 'latest',
   },
   rules: {
-    'vue/comment-directive': 'off'
-  }
+    'vue/comment-directive': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+  },
 }
